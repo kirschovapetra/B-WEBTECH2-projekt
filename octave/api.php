8c34af3f5890d67b8371905970298b89ee642607
pepak1234 <kirschovapetra@gmail.com>
2020-05-24 14:37:32 +0200

    komenty

--- octave/api.php
+++ octave/api.php
@@ -1,11 +1,15 @@
 <?php
+/*********************************** Octave api [Matus, Simona, Petra, Veronika] **************************************/
+
 include "../config.php";
 
 header("Content-Type:application/json");
 
+//data na animacie
 if (isset($_GET["execute"]) && $_GET["execute"] == "animation") {
     executeAnimation();
 }
+//spustanie normalnych prikazov
 else if (isset($_GET["execute"]) && $_GET["execute"] == "command") {
     executeCommand();
 }
@@ -19,7 +23,7 @@ function executeCommand(){
 function  executeAnimation(){
 
     if (isset($_GET["type"])) {
-        //Petra
+        // [Petra]
         if ($_GET["type"] == "ballbeam") {
             getBallBeamData();
         }
@@ -36,70 +40,77 @@ function  executeAnimation(){
     }
 }
 
-
-function logStatus($command,$status){
-    global $db;
-    $timestamp = date("Y-m-d H:i:s");
-    $query = "";
-    if ($status == false){
-        $query = "INSERT INTO logs(timestamp,command,status)
-                      VALUES('$timestamp','$command','success')";
-    }
-    else {
-        $query = "INSERT INTO logs(timestamp,command,status,error_info)
-                    VALUES('$timestamp','$command','error','nepodarilo sa vykonat prikaz')";
-    }
-    $db->exec($query);
-}
-
+//gulicka na tyci [Petra]
 function getBallBeamData(){
     global $path;
 
     if (isset($_GET["position"]) && isset($_GET["newInput"])) {
-        $r = $_GET['position'];
-        $newInput = json_decode($_GET['newInput']);
-        $command = "octave $path/gulicka.m $r $newInput[0] $newInput[1] $newInput[2] $newInput[3]";
-        $output = null;
-        exec($command, $output, $returnVal);
+        $r = $_GET['position']; //nova pozicia
+        $newInput = json_decode($_GET['newInput']); //x(size(x,1),:) z predosleho volania
 
-        logStatus($command,empty($output));
+        //spustenie prikazu v octave
+        $command = "octave $path/gulicka.m $r $newInput[0] $newInput[1] $newInput[2] $newInput[3]";
+        exec($command, $octaveOutput, $returnVal);
+
+        //zapis logu do databazy
+        logStatus($command,empty($octaveOutput));
 
         $positions = array();
         $times = array();
         $angles = array();
-
-
-
         $newInput = array();
-        foreach ($output as $id => $row) {
 
+        //data ziskane z octave
+        foreach ($octaveOutput as $id => $row) {
+            //x(size(x,1),:)  -> zapise sa do $newInput
             if ($id>=0 && $id<=3){
                 array_push($newInput,floatval(trim($row)));
             }
+            //pozicie, casy, uhly
             else {
-                $splitRow = preg_split('/\s+/', trim($row));
+                $splitRow = preg_split('/\s+/', trim($row)); //odstranenie medzier
+
                 if (isset($splitRow[0]) && !empty($splitRow[0])) {
-                    array_push($positions, floatval($splitRow[0]));
+                    array_push($positions, floatval($splitRow[0])); //pridanie novej pozicie
                 }
                 if (isset($splitRow[1]) && !empty($splitRow[1])) {
-                    array_push($times, floatval($splitRow[1]));
+                    array_push($times, floatval($splitRow[1])); //pridanie noveho casu
                 }
                 if (isset($splitRow[2]) && !empty($splitRow[2])) {
-                    array_push($angles, floatval($splitRow[2]));
+                    array_push($angles, floatval($splitRow[2])); //pridanie noveho uhlu
                 }
             }
         }
 
+        //vystup ako asociativne pole
         $out = array();
         $out["positions"] = $positions;
         $out["times"] = $times;
         $out["angles"] = $angles;
         $out["newInput"] = $newInput;
 
+        //vypis
         echo json_encode($out);
     }
 }
 
+//zapis logov do databazy [Petra]
+function logStatus($command,$status){
+    global $db;
+    $timestamp = date("Y-m-d H:i:s");
+
+    //prazdny vystup z octave
+    if ($status == false){
+        $query = "INSERT INTO logs(timestamp,command,status)
+                      VALUES('$timestamp','$command','success')";
+    }
+    //octave vratil nejake data
+    else {
+        $query = "INSERT INTO logs(timestamp,command,status,error_info)
+                    VALUES('$timestamp','$command','error','nepodarilo sa vykonat prikaz')";
+    }
+    $db->exec($query);
+}
 
 
 
