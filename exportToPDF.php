f82f5960c8d7d20d28e5cf731087ca8271e295ad
pepak1234 <kirschovapetra@gmail.com>
2020-05-27 22:47:06 +0200

    sk/en logy

--- exportToPDF.php
+++ exportToPDF.php
@@ -7,18 +7,34 @@ require 'fpdf/fpdf.php';
 
 //https://phpflow.com/php/generate-pdf-file-mysql-database-using-php/
 class PDF extends FPDF {
+    private $lang; //jazyk stranky
 
-    function Header()    {
+    public function __construct($lang) {
+        parent::__construct("L"); //landscape orientacia stranky
+        $this->lang = $lang;
+    }
+
+    public function Header()    {
         $this->SetFont('Arial','B',13);
         $this->Cell(100);
-        $this->Cell(80,10,'Logy',1,0,'C');
+        if ($this -> lang == "en"){
+            $this->Cell(80,10,'Logs',1,0,'C');
+        }
+        else if ($this -> lang == "sk") {
+            $this->Cell(80,10,'Logy',1,0,'C');
+        }
         $this->Ln(20);
     }
 
-    function Footer() {
+    public function Footer() {
         $this->SetY(-15);
         $this->SetFont('Arial','I',8);
-        $this->Cell(0,10,'Strana '.$this->PageNo().'/{nb}',0,0,'C');
+        if ($this -> lang == "en"){
+            $this->Cell(0,10,'Page '.$this->PageNo().'/{nb}',0,0,'C');
+        }
+        else if ($this -> lang == "sk") {
+            $this->Cell(0,10,'Strana '.$this->PageNo().'/{nb}',0,0,'C');
+        }
     }
 
     //rozdelenie dlheho textu v bunke do viacerych riadkov
@@ -36,33 +52,45 @@ class PDF extends FPDF {
     }
 }
 
-//pripojenie do databazy
-$query = $db->query("SELECT * FROM logs ORDER BY id ASC");
-$rows = $query->fetchAll();
+//jazyk stranky
+$language = $_GET["lang"];
 
-//inicializacia pdf
-$pdf = new PDF("L");
-$pdf->AddPage();
-$pdf->AliasNbPages();
-$pdf->SetFont('Arial','B',8);
+if (isset($language)) {
 
-//zapis headeru do pdf
-$pdf->Cell(5, 5, "id", 1);
-$pdf->Cell(30, 5,"timestamp", 1);
-$pdf->Cell(180, 5, "command", 1);
-$pdf->Cell(15, 5, "status", 1);
-$pdf->Cell(50, 5, "eorror_info", 1);
+    //pripojenie do databazy
+    $query = $db->query("SELECT * FROM logs ORDER BY id ASC");
+    $rows = $query->fetchAll();
 
-//zapis riadkov do pdf
-foreach($rows as $row) {
-    $pdf->Ln();
-    $pdf->Cell(5, 20, $row["id"], 1);
-    $pdf->Cell(30, 20, $row["timestamp"], 1);
-    //viacriadkova bunka s paddingom
-    $pdf->MultiAlignCell( 180, 4, str_pad(trim($row["command"]), 850), 1);
-    $pdf->Cell(15, 20, $row["status"], 1);
-    $pdf->Cell(50, 20, $row["error_info"], 1);
+    //inicializacia pdf
+    $pdf = new PDF($language);
+    $pdf->AddPage();
+    $pdf->AliasNbPages();
+    $pdf->SetFont('Arial','B',8);
+
+    //zapis headeru do pdf
+    $pdf->Cell(5, 5, "id", 1);
+    $pdf->Cell(30, 5,"timestamp", 1);
+    $pdf->Cell(180, 5, "command", 1);
+    $pdf->Cell(15, 5, "status", 1);
+    $pdf->Cell(50, 5, "error_info", 1);
+
+    //zapis riadkov do pdf
+    foreach($rows as $row) {
+        $pdf->Ln();
+        $pdf->Cell(5, 20, $row["id"], 1);
+        $pdf->Cell(30, 20, $row["timestamp"], 1);
+        //viacriadkova bunka s paddingom
+        $pdf->MultiAlignCell( 180, 4, str_pad(trim($row["command"]), 850), 1);
+        $pdf->Cell(15, 20, $row["status"], 1);
+        $pdf->Cell(50, 20, $row["error_info"], 1);
+    }
+
+    //zapis vystupu do pdf, otvori sa nova stranka
+    if ($language == "en"){
+        $pdf->Output("logs_" . date('Y-m-d') . ".pdf",'I');
+    }
+    else if ($language == "sk") {
+        $pdf->Output("logy_" . date('Y-m-d') . ".pdf",'I');
+    }
 }
-//zapis vystupu do pdf, otvori sa nova stranka
-$pdf->Output("logs_" . date('Y-m-d') . ".pdf",'I');
 ?>
